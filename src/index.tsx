41f0c36c454daa22a48db8d3d159335b796c0951
Jakub Bednár <j.bednar1997@gmail.com>
2025-02-27 11:10:16 +0100

    Cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,12 @@
 import r2wc from "@r2wc/react-to-web-component";
-import React from "react";
 import { ImageUploader } from "./components/ImageUploader";
-// Example React Components
-const Button = ({ label }: { label: string }) => <button>{label}</button>;
-const Alert = ({ message }: { message: string }) => (
-    <div style={{ color: "red" }}>{message}</div>
-);
 
 // Convert to Web Components
-const WebButton = r2wc(Button, { props: { label: "string" } });
-const WebAlert = r2wc(Alert);
 const WebImageUploader = r2wc(ImageUploader, {
     props: {
         onProcessed: "function",
     },
 });
 // Register them
-customElements.define("web-button", WebButton);
-customElements.define("web-alert", WebAlert);
+
 customElements.define("image-uploader", WebImageUploader);
