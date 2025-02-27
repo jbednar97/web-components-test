713b2f2e781e666f5884bc6240bcb02871f87a70
Jakub Bednár <j.bednar1997@gmail.com>
2025-02-27 10:44:22 +0100

    Fix deps

--- build.js
+++ build.js
@@ -11,5 +11,6 @@ esbuild
         outfile: "dist/web-components.js",
         minify: true,
         sourcemap: true,
+        external: ["@uppy/utils/lib/emitSocketProgress"],
     })
     .catch(() => process.exit(1));
